3b3d345c737de3ac4c719f31492171c9ff186f05
koval23 <ukoval400@gmail.com>
2025-02-06 12:42:51 +0100

    0206Commit

--- planner-users/src/main/java/ru/javabegin/micro/planner/users/controller/AuthController.java
+++ planner-users/src/main/java/ru/javabegin/micro/planner/users/controller/AuthController.java
@@ -3,12 +3,14 @@ package ru.javabegin.micro.planner.users.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClient;
 import ru.javabegin.micro.planner.users.dto.UserRegister;
 import ru.javabegin.micro.planner.users.service.AuthService;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
@@ -17,11 +19,46 @@ public class AuthController {
 
     private final AuthService authService;
 
+
     @PostMapping("/register")
     public ResponseEntity<Void> register(@RequestBody UserRegister userRegister) {
-        System.out.println("я в контроллере");
+        System.out.println("я в контроллере register");
         authService.registration(userRegister);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping("/redirect")
+    public ResponseEntity<Map<String, String>> handleRedirect(
+            @RequestParam("code") String code) {
+        System.out.println("я в контроллере redirect");
+
+        WebClient webClient = WebClient.builder().baseUrl("http://localhost:8080").build();
+
+        Map<String, String> tokens = webClient.post()
+                .uri("/realms/todoapp-realm/protocol/openid-connect/token")
+                .bodyValue(getTokenRequestBody(code))
+                .retrieve()
+                .bodyToMono(Map.class)
+                .block(); // Блокируем для получения синхронного ответа
+
+        System.out.println(tokens.get("access_token"));
+
+        if (tokens != null) {
+            // Отправляем токены на фронтенд
+            return ResponseEntity.ok(tokens);
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+    }
+
+    private MultiValueMap<String, String> getTokenRequestBody(String code) {
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        map.add("grant_type", "authorization_code");
+        map.add("client_id", "todoapp-client"); // Заменить на свой client_id
+        map.add("client_secret", "UTrqFVsKShdSC9ClXxNEVaXsJYJav47V"); // Секретный ключ клиента
+        map.add("code", code); // Код из редиректа
+        map.add("redirect_uri", "http://localhost:8765/planner-users/auth/redirect"); // Должен совпадать с тем, что в Keycloak
+        return map;
     }
 
 }
--- planner-users/src/main/java/ru/javabegin/micro/planner/users/service/AuthService.java
+++ planner-users/src/main/java/ru/javabegin/micro/planner/users/service/AuthService.java
@@ -1,9 +1,15 @@
 package ru.javabegin.micro.planner.users.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import ru.javabegin.micro.planner.users.dto.UserRegister;
 import ru.javabegin.micro.planner.users.keycloak.KeycloakUtils;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -12,7 +18,38 @@ public class AuthService {
     public final AdminService adminService;
     public final KeycloakUtils keycloakUtils;
 
-    private final String REGISTR_URL = "http://localhost:8080/realms/todoapp-realm/protocol/openid-connect/auth?response_type=code&client_id=todoapp-client&state=sidyuf8s67dfisdgf&scope=openid phone&redirect_uri=https://localhost:8080/redirect";
+    @Value("${keycloak.client.redirect}")
+    private String redirectUri;
+    @Value("${keycloak.get.token.url}")
+    private String keycloakTokenUrl;
+    @Value("${keycloak.client}")
+    private String clientID;
+    @Value("${keycloak.client.secret}")
+    private String clientSecret;
+
+
+    // Настройки из файла properties
+    public String getToken(String code) {
+
+        // 3. Запрашиваем Access Token с использованием кода авторизации
+        RestTemplate restTemplate = new RestTemplate();
+        Map<String, String> requestBody = new HashMap<>();
+        requestBody.put("grant_type", "authorization_code");
+        requestBody.put("client_id", clientID);
+        requestBody.put("client_secret", clientSecret);
+        requestBody.put("code", code);
+        requestBody.put("redirect_uri", redirectUri);
+
+        ResponseEntity<Map> response = restTemplate.postForEntity(keycloakTokenUrl, requestBody, Map.class);
+
+        // 4. Извлекаем Access Token из ответа
+        if (response.getStatusCode().is2xxSuccessful() && response.getBody() != null) {
+            return (String) response.getBody().get("access_token");
+        } else {
+            throw new RuntimeException("Не удалось получить токен. Ответ: " + response);
+        }
+    }
+
 
     public void registration(UserRegister userRegister) {
         adminService.addUser(userRegister);
